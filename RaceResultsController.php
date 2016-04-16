ebc0f2fb5e5d55e6d43734641d6091aad73d8509
Emlyn Christie <emlyn.christie@gmail.com>
2016-04-16 14:34:05 +0000

    added RaceResultsController.php

---
+++ RaceResultsController.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\raceresults\Controller\RaceResultsController.
+ */
+
+namespace Drupal\raceresults\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class RaceResultsController extends ControllerBase {
+  public function content() {
+    return array(
+        '#type' => 'markup',
+        '#markup' => $this->t('Hello, Race Results!'),
+    );
+  }
+}
+
+?>
